c6e0bfb3f99d10bbb7a0d8f0845111d72dc8e4aa
MarkoCvijeticRA40 <93485029+MarkoCvijeticRA40@users.noreply.github.com>
2023-05-10 20:33:30 +0200

    Implemented new microservice communication

--- Backend/AccommodationService/Service/ServiceAccommodation.cs
+++ Backend/AccommodationService/Service/ServiceAccommodation.cs
@@ -1,12 +1,21 @@
 ﻿using AccommodationService.Model;
 using AccommodationService.Repository;
+using Grpc.Core;
 using Grpc.Net.Client;
+using ProtoService1;
 
 namespace AccommodationService.Service
 {
     public class ServiceAccommodation : IAccommodationService
     {
         private readonly IAccommodationRepository _accommodationRepository;
+
+        /* Ovako se poziva mikroservis kada zelite da dobavite usera
+        string id = "string";
+        var channel = new Channel("localhost", 4111, ChannelCredentials.Insecure);
+        var client = new UserGrpc.UserGrpcClient(channel);
+        var user = client.GetUserInfo(new UserRequest { Id = id });   
+        */
 
         public ServiceAccommodation(IAccommodationRepository repository)
         {
@@ -19,6 +28,12 @@ namespace AccommodationService.Service
 
         public IEnumerable<Accommodation> GetAll()
         {
+            //Ovako se poziva mikroservis,obrisite slobodono kad b
+            string id = "6426ea62c3414f9576ca5a43";
+            var channel = new Channel("localhost", 4112, ChannelCredentials.Insecure);
+            var client = new UserGrpc.UserGrpcClient(channel);
+            var user = client.GetUserInfo(new UserRequest { Id = id });
+            //
             return _accommodationRepository.GetAll();
         }
 
--- Backend/AccommodationService/Startup.cs
+++ Backend/AccommodationService/Startup.cs
@@ -27,6 +27,7 @@ namespace AccommodationService
             services.AddScoped<IAccommodationRepository, AccommodationRepository>();
             services.AddScoped<IAccommodationService, ServiceAccommodation>();
             services.AddScoped<IGenericMapper<Accommodation, AccommodationDTO>, AccommodationMapper>();
+            services.AddScoped<IUserService, UserService>();
             services.AddGrpc();
             // Add Swagger
             services.AddSwaggerGen(c =>
